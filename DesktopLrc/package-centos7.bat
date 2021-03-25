d64cec05e5c3b126468deb091c711a0d7b30703e
钢翼 <659240788@qq.com>
2021-03-25 17:33:39 +0800

    新建avalonia 项目

---
+++ DesktopLrc/package-centos7.bat
@@ -0,0 +1,2 @@
+dotnet publish -r centos.7-x64 -c Release  /p:PublishSingleFile=true  /p:PublishTrimmed=true 
+pause
---
+++ DesktopLrc/package-ubuntu17.04.bat
@@ -0,0 +1,2 @@
+dotnet publish -r ubuntu.17.04-x64 -c Release  /p:PublishSingleFile=true  /p:PublishTrimmed=true 
+pause
